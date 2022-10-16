3c6f1fdafbb8d3834af6df58658ccec1a5d59ee7
Diana Vu <aznalive@hotmail.com>
2022-10-15 23:46:46 -0400

    added port, requires for express, GET and POST for note/index routes

---
+++ routes/indexroute.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3001
+
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get()
+
+app.listen(PORT, () => 
+console.log(`now listening at http://localhost:${PORT}`))
--- server.js
+++ server.js
@@ -1,3 +1,37 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
+const PORT = 3001
+const data = require('./db/db.json');
+const fs = require('fs');
+
+app.get('/api/db', (req, res) => res.json(data));
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+
+
+app.get('/api/notes', (req, res) => {
+    res.json(`${req.method} test`);
+});
+
+
+app.post('/api/notes', (req, res) => {
+    res.json(`${req.method} has been posted!`);
+});
+
+
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+)
+
+app.listen(PORT, () => console.log(`now listening at http://localhost:${PORT}`));
+
+
+
+
 
