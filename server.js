9bf49c1efcd0b533ce86edad80fa0fe690dc0943
Diana Vu <aznalive@hotmail.com>
2022-10-17 18:31:38 -0400

    test 2

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ app.use(express.urlencoded({ extended: true }));
 app.use('/api', noteRoutes);
 app.use('/', htmlRoutes);
 
-app.listen(process.env.PORT || 5000);
+app.listen(PORT, () => console.log(`now listening at http://localhost:${PORT}`));
