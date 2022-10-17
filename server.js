145438aa870fbc805bdec895c266c25a20095d71
Diana Vu <aznalive@hotmail.com>
2022-10-17 19:25:23 -0400

    noteroute wasnt being updated to noteRoute

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = 3001
 const fs = require('fs')
 //api routes
 const htmlRoutes = require('./routes/htmlRoute')
-const noteRoutes = require('./routes/notechangeRoute')
+const noteRoutes = require('./routes/noteRoute')
 
 app.use(express.static('public'))
 // app.use(express.static('db'));
